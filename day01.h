2bd01e201d4fc6f7276b75266241a3805a5badbb
Alex Oxorn <alex.oxorn@gmail.com>
2021-12-02 00:01:03 -0500

    Day 1

---
+++ day01.h
@@ -0,0 +1,15 @@
+//
+// Created by alexoxorn on 2021-12-01.
+//
+
+#ifndef ADVENTOFCODE2021_DAY01_H
+#define ADVENTOFCODE2021_DAY01_H
+
+namespace day01 {
+    extern const char* input;
+    extern const char* sample_input;
+    void puzzle1();
+    void puzzle2();
+}
+
+#endif //ADVENTOFCODE2021_DAY01_H
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "day01.h"
+
+int main() {
+    day01::puzzle1();
+    day01::puzzle2();
+    return 0;
+}
