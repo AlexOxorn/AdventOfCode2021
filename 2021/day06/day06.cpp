35256e6fee8ac41ce1bc3db75a49bf5bc1950f61
Alex Oxorn <alex.oxorn@gmail.com>
2021-12-06 20:24:31 -0500

    Day 6

---
+++ 2021/day06/day06.cpp
@@ -0,0 +1,67 @@
+//
+// Created by alexoxorn on 2021-12-06.
+//
+
+#include "day06.h"
+#include <limits>
+#include <numeric>
+#include <ranges>
+
+#define YEAR 2021
+#define DAY 06
+
+namespace day06 {
+    using count_type = long;
+
+    class fish_population {
+        std::array<count_type, 9> days_count{};
+        int day = 0;
+    public:
+        explicit fish_population(std::istream& in) {
+            int i;
+            while(in >> i) {
+                days_count.at(i) += 1;
+                in.ignore(std::numeric_limits<std::streamsize>::max(), ',');
+            }
+        }
+
+        fish_population& operator++() {
+            day++;
+            std::rotate(days_count.begin(), days_count.begin() + 1, days_count.end());
+            days_count.at(6) += days_count.at(8);
+            return *this;
+        }
+
+        void print_state() {
+            switch (day) {
+                case 0:
+                    printf("Initial State:  ");
+                    break;
+                case 1:
+                    printf("After   1 day:  ");
+                    break;
+                default:
+                    printf("After %3d days: ", day);
+            }
+            std::cout << std::accumulate(days_count.begin(), days_count.end(), count_type{});
+            printf(" Total\n");
+        }
+    };
+
+    void run_test(int generations) {
+        auto input = GET_STREAM(input, int);
+        fish_population fishes(input);
+        for(int a : stdv::iota(0, generations)) {
+            ++fishes;
+        }
+        fishes.print_state();
+    }
+
+    void puzzle1() {
+        run_test(80);
+    }
+
+    void puzzle2() {
+        run_test(256);
+    }
+}
--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 #include "2021/day03/day03.h"
 #include "2021/day04/day04.h"
 #include "2021/day05/day05.h"
+#include "2021/day06/day06.h"
 #include <cstdio>
 
 int main() {
     printf("Day 05:\n");
-    day05::puzzle1();
-    day05::puzzle2();
+    day06::puzzle1();
+    day06::puzzle2();
     printf("\n");
     return 0;
 }
